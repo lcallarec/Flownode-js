afa4c69d573b0a12dc43f2f55400b10fcf78b352
lcallarec <l.callarec@gmail.com>
2012-11-02 23:38:04 +0100

    Push of demo pages

---
+++ demo/index.php
@@ -0,0 +1,74 @@
+<?php
+ini_set('error_reporting', E_ALL);
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+use SwitchBoard\SwitchBoard;
+use SwitchBoard\Channel;
+use SwitchBoard\Response;
+
+require_once dirname(__FILE__).'/../server/php/ext/Symfony2/Component/ClassLoader/UniversalClassLoader.php';
+
+$loader = new UniversalClassLoader();
+
+$loader->registerNamespaces(array(
+       'Symfony'     => __DIR__.'/../server/php/ext/Symfony2',
+       'SwitchBoard' => __DIR__.'/../server/php/lib'
+));
+
+$loader->register();
+
+$data = array('open.remote.modal.closure'  => array('erk' => 'blue'),
+              'open.remote.modal.function' => array('erk' => 'blue'),
+              'open.remote.modal.static'   => array('erk' => 'blue'),
+              'open.remote.modal.instance' => array('erk' => 'blue'));
+
+$sb = SwitchBoard::get();
+
+$ext = 1;
+
+
+
+$closure =  function($response, $data) use ($ext) {
+
+  $d['test'] = 5;
+  return  new Channel('open.remote.modal.closure', $d);
+
+};
+
+$sb->register('open.remote.modal.closure', $closure);
+
+
+$function =  function($response, $data) {
+  $d['test'] = 6;
+  return  new Channel('open.remote.modal.function', $d);
+};
+
+$sb->register('open.remote.modal.function', $function);
+
+class MyClass
+{
+    static public function staticMethod($response, $data)
+    {
+        $d['test'] = 7;
+        return new Channel('open.remote.modal.static', $d);
+    }
+
+    public function instanceMethod($response, $data)
+    {
+        $d['test'] = 8;
+        return new Channel('open.remote.modal.instance', $d);
+    }
+}
+
+new Response();
+
+$myClass = new MyClass();
+
+$sb->register('open.remote.modal.static', array('MyClass', 'staticMethod'));
+$sb->register('open.remote.modal.instance', array($myClass, 'instanceMethod'));
+
+$response = $sb->connect($data);
+
+echo $response->toJSON();
+
+
+
