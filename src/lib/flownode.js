a955e2297fb97ebbe0a2dc7dc1cbecc03480fc1c
lcallarec <l.callarec@gmail.com>
2014-05-02 23:46:31 +0200

    Adding JSDoc and code cleaning

--- src/lib/flownode.js
+++ src/lib/flownode.js
@@ -1,287 +1,302 @@
 if(typeof Flownode === 'undefined') {
     Flownode = {};
 }
-
 /**
-* Manage array like indexed and / or associative
-*
-* @param Array collection
-**/
+ * Manage array like indexed and / or associative
+ * @param {Array} collection
+ * @constructor
+ **/
 Flownode.Collection = function(collection) {
 
     "use strict";
 
     /**
-    *
-    *
-    **/
+     * @private
+     * @type {Array}
+     **/
     var _collection = collection;
 
     /**
-    *
-    *
-    **/
+     * @private
+     * @type {Object}
+     **/
     this._nameKeyMap = {};
 
     /**
-	* Internal cursor position
-	* By default, it is on last position
-	*
-	* @var integer _i
-	**/
-	this._i = 0;
-
-   /**
-	* Nomber of elements in the collection
-	* @var integer length
-	**/
-	this.length = _collection.length;
-
-	var i = 0;
-	for(element in _collection) {
-		this._nameKeyMap[element.toString()] = i;
-		i++;
-	}
-
-	/**
-	* Add an object to the collection
-	*
-	* @param string|integer name   Unique key for this object ; may be a string or an integer
-	* @param object  		 object	object to store in the collection
-	**/
-	this.add = function(name, object) {
-
-    _collection.push(object);
+     * Internal cursor position
+     * By default, it is on last position
+     *
+     * @type {Number}
+     **/
+    this._i = 0;
 
     /**
-    * Used to retrieve easily object in collection by its name
+     * Number of elements in the collection
+     * @type {Number} collection length
+     **/
+    this.length = _collection.length;
 
-    * @see this.getByName()
-    **/
-    this._nameKeyMap[name] = _collection.length - 1;
-	};
+    var i = 0;
+    for(var element in _collection) {
+        this._nameKeyMap[element.toString()] = i;
+        i++;
+    }
 
-	/**
-	*
-	* Retrieve an object in the collection by its name
-	*
-	* @param string|integer	name	Unique name
-	**/
-	this.getByName = function(name) {
+    /**
+     * Add an object to the collection
+     * @function
+     * @param {String|Number} name   Unique key for this object ; may be a string or an integer
+     * @param {Object}  	   object Object to store in the collection
+     * @returns {void}
+     **/
+    this.add = function(name, object) {
 
-		var element = _collection[this._nameKeyMap[name]];
+        _collection.push(object);
 
-		if(typeof element === 'undefined') {
-			return null;
-		}
+        /**
+         * Used to retrieve easily object in collection by its name
+         * @see this.getByName()
+         **/
+        this._nameKeyMap[name] = _collection.length - 1;
+    };
 
-		return _collection[this._nameKeyMap[name]];
-	};
+    /**
+     * Retrieve an object in the collection by its name
+     * @function
+     * @param {String|Number}	name	Unique name
+     * @returns {*}
+     **/
+    this.getByName = function(name) {
 
-	/**
-	* Cursor is move to next position
-	*/
-	this.next = function() {
-		return _collection[this._i++];
-	};
+        var element = _collection[this._nameKeyMap[name]];
 
-	/**
-	*
-	**/
-	this.previous = function() {
-		return _collection[this._i--];
-	};
+        if(typeof element === 'undefined') {
+            return null;
+        }
 
-	/**
-	*
-	**/
-	this.current = function() {
-		return _collection[this._i];
-	};
+        return _collection[this._nameKeyMap[name]];
+    };
 
-	/**
-	**/
-	this.isEmpty = function() {
-		return this.length !== 0;
-	};
+    /**
+     * Cursor is move to next position
+     * @function
+     * @returns {*}
+     */
+    this.next = function() {
+        return _collection[this._i++];
+    };
 
+    /**
+     * @function
+     * @returns {*}
+     **/
+    this.previous = function() {
+        return _collection[this._i--];
+    };
+
+    /**
+     * @function
+     * @returns {*}
+     */
+    this.current = function() {
+        return _collection[this._i];
+    };
+
+    /**
+     * @function
+     * @returns {boolean}
+     */
+    this.isEmpty = function() {
+        return this.length !== 0;
+    };
+
+};
+/**
+ * @object
+ */
+Flownode = {
+
+    /**
+     * @type {String} Request / Query / Response namespace
+     */
+    namespace : 'response',
+
+    /**
+     * Channels are a set of named callback functions
+     *
+     * @type {Flownode.Collection} Collection
+     **/
+    channels : new Flownode.Collection([]),
+
+    /**
+     * Register a callback in channels
+     * @function
+     * @param {String} 		name 		Fully qualified (and unique) callback name
+     * @param {Function}  	callback	Callback function executed by fire* methods
+     * @returns {void}
+     **/
+    register : function(name, callback) {
+        this.channels.add(name, callback);
+    },
+
+    /**
+     * Get a register channel callback function
+     * @function
+     * @param {String}		name 		Fully qualified (and unique) callback name
+     * @returns {Function}
+     **/
+    getCallbackByChannel : function(name) {
+        return this.channels.getByName(name);
+    },
+
+    /**
+     * Fire the specified callback for this channel
+     * @function
+     * @param {String}  channel
+     * @param {*}       data
+     * @returns {void}
+     **/
+    fire : function(channel, data) {
+        if(typeof data === 'undefined') {
+            data = {};
+        }
+
+        var callback = this.getCallbackByChannel(channel);
+        if(null !== callback) {
+            callback.call(this, data);
+        }
+
+    },
+
+    /**
+     * Fire all callbacks for all channels
+     * @function
+     * @param {String}  channel
+     * @param {*}       data
+     * @returns {void}
+     **/
+    fireAll : function(data) {
+        $.each(data, function(i, data) {
+            this.fire(data.channel, data.command);
+        });
+    }
 };
 
 /**
-* Core
-*
-*
-**/
-Flownode.Switchboard = {
+ *
+ * @param url
+ * @param data
+ * @constructor
+ */
+Flownode.Xhr = function(url, data) {
 
-    namespace : 'switchboard',
-
-	/**
-	* Channels are a set of named callback functions
-	*
-	* @ var Collection channel
-	**/
-	channels : new Flownode.Collection([]),
-
-	/**
-	* Register a callback in channels
-	*
-	* @param string  		name 		Fully qualified (and unique) callback name
-	* @param functions  	callback	Callback function executed by exec() method
-	**/
-	register : function(name, callback) {
-		this.channels.add(name, callback);
-	},
-
-	/**
-	* Register a callback in channels
-	*
-	* @param string  		name 		Fully qualified (and unique) callback name
-	* @param functions  	callback	Callback function executed by exec() method
-	**/
-	getChannel : function(name) {
-		return this.channels.getByName(name);
-	},
-        /**
-	*
-	* @param
-	* @param
-	**/
-	fire : function(channel, data) {
-		if(typeof data === 'undefined') {
-			var data = {};
-		}
-
-		var command = this.getChannel(channel);
-		if(null !== command) {
-			command.call(this, data);
-		}
-
-	},
-
-   /**
-	*
-	**/
-	fireAll : function(data) {
-		$.each(data, function(i, data) {
-			this.fire(data.channel, data.command);
-		});
-
-	}
-};
-
-Flownode.Switchboard.Xhr = function(url, data) {
-
-    var that  = this;
-
-    var sb    = Flownode.Switchboard;
-
-    Flownode.Xhr.call(this, url, data);
+    Flownode._Xhr.call(this, url, data);
 
     this.__send = this.send;
 
     /**
-    *
-    * @param string 	type
-    * @param string 	url
-    * @param Object		data
-    * @param Object 	callbacks	{dones: [], fails: []}
-    **/
+     * @function
+     * @param {Object}	data
+     * @param {Object} 	callbacks	({done: {}, fail: {}})
+     * @param {String} method       http method
+     **/
     this.send = function(data, callbacks, method) {
 
         this.promise = this.__send(data, callbacks, method);
 
         this.promise.done(function(response, status, jqXHR){
 
-            for(state in response[sb.namespace]) {
+            for(var state in response[Flownode.namespace]) {
 
-                    switch(state) {
-                        case 'success':
-                            sb.Xhr.onSuccess(response, status, jqXHR);
-                         break;
-                        case 'failure':
-                            sb.Xhr.onFailure(response, status, jqXHR);
-                        default:
-                         break;
-                    }
-
-                    for(command in response[sb.namespace][state]) {
-                        sb.fire(command, response[sb.namespace][state][command]);
-                    }
-
+                switch(state) {
+                    case 'success':
+                        Flownode.Xhr.onSuccess(response, status, jqXHR);
+                        break;
+                    case 'failure':
+                        Flownode.Xhr.onFailure(response, status, jqXHR);
+                    default:
+                        break;
                 }
 
+                for(var callback in response[Flownode.namespace][state]) {
+                    Flownode.fire(callback, response[Flownode.namespace][state][callback]);
+                }
+
+            }
+
         }).fail(function(jqXHR, status, error) {
-            sb.Xhr.onResponseError(jqXHR, status, error);
+            Flownode.Xhr.onResponseError(jqXHR, status, error);
         });
 
         return this.resolve({dones: [], fails: []});
 
     };
-};
-
-Flownode.Switchboard.Xhr.onResponseError = function(jqXHR, status, error){
 
 };
 
-Flownode.Switchboard.Xhr.onSuccess = function(response, status, jqXHR){
+Flownode.Xhr.onResponseError = function(jqXHR, status, error){};
+Flownode.Xhr.onSuccess = function(response, status, jqXHR){};
+Flownode.Xhr.onFailure = function(response, status, jqXHR){};
 
-};
-Flownode.Switchboard.Xhr.onFailure = function(response, status, jqXHR){
+Flownode._Xhr = function(url, data) {
 
-};
-
-Flownode.Xhr = function(url, data) {
-
+    /**
+     * @private
+     * @type {String}
+     */
     var url    = url;
+
+    /**
+     * @private
+     * @type {Object}
+     */
     var data   = $.extend(data, {});
 
-   /**
-    *
-    * @param string 	type
-    * @param string 	url
-    * @param Object		data
-    * @param Object 	callbacks	{done: {}, fail: {}}
-    **/
+    /**
+     * @function
+     * @private
+     * @param {Object}	data
+     * @param {Object} 	callbacks	({done: {}, fail: {}})
+     * @param {String} method       http method
+     **/
     this.send = function(data, callbacks, method) {
 
         this.callbacks = $.extend({dones: [], fails: []}, callbacks);
         this.data      = $.extend(data, this.data);
-        
-        if(method !== 'undefined') {
-            this.method = 'GET';
-        } else {
-            if(typeof data == 'string') {
-                this.method = 'GET';
-            } else {
-                this.method = 'POST';
-            }
-        }
+
+        this.method    = method !== 'undefined' ? method : 'GET';
 
         this.promise = $.ajax({
             type: this.method,
             url: url,
-            data: data
+            data: data,
+            dataType: 'json'
         });
 
         return this.promise;
     };
 
+    /**
+     * Resolve the response
+     * @function
+     * @private
+     * @param {Array} callbacks functions
+     * @returns {Flownode._Xhr}
+     */
     this.resolve = function(callbacks) {
 
         this.callbacks = $.extend(callbacks, this.callbacks);
 
         for(var i in this.callbacks.dones) {
-          this.promise.done(this.callbacks.dones[i]);
+            this.promise.done(this.callbacks.dones[i]);
         }
 
         for(var i in this.callbacks.fails) {
-          this.promise.fail(this.callbacks.fails[i]);
+            this.promise.fail(this.callbacks.fails[i]);
         }
 
         return this;
     };
 
     return this;
-
 };
